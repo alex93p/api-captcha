191007c976321680acdfb4d8b6351b61f6170765
apompili <alexander.pompili@soprasteria.com>
2023-11-23 16:05:43 +0100

    feat: interceptor

---
+++ src/interceptors/add-data-transform/add-data-transform.interceptor.ts
@@ -0,0 +1,11 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Response } from 'express';
+
+@Injectable()
+export class AddDataTransformInterceptor<T> implements NestInterceptor<T, Response<T>> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
+    return next.handle().pipe(map(data => (data === undefined || data?.data !== undefined ? data : { data })));
+  }
+}
